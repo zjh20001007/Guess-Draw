dda10b77bb77508ddfb36cf670faed27eb39c0fe
张隽涵 <123@qq.com>
2022-04-01 15:10:41 +0800

    你画我猜项目创建

---
+++ src/test/java/com/example/guessanddraw/GuessAndDrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guessanddraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessAndDrawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
