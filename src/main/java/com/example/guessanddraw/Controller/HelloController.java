a927e4a45cf3e09c4405e441a1f89848c30eb7d2
张隽涵 <123@qq.com>
2022-04-01 15:47:42 +0800

    你画我猜项目后端创建

--- src/main/java/com/example/guessanddraw/Controller/HelloController.java
+++ src/main/java/com/example/guessanddraw/Controller/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+    //初始
     @ResponseBody
     @RequestMapping("/hello")
     public String hello() {
